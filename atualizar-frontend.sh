8681a97ec23ac6288fff7c37615236b756d75257
Andre Cardoso <andre@vmi2566214.contaboserver.net>
2025-05-05 23:07:49 +0200

    Backup automático: 2025-05-05 23:07

--- atualizar-frontend.sh
+++ atualizar-frontend.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-echo "🔧 A fazer build do React..."
+echo "🔧 A fazer build do Vite..."
 cd ~/projetos/cv-projecto/frontend || exit 1
 npm run build
 
 echo "📁 A copiar nova build para o NGINX..."
-sudo rm -rf /var/www/pre-bolsa-frontend/*
-sudo cp -r dist/* /var/www/pre-bolsa-frontend/
+sudo rm -rf /var/www/cv-frontend/*
+sudo cp -r dist/* /var/www/cv-frontend/
 
-echo "✅ Frontend atualizado com sucesso!"
-
+echo "✅ Frontend do CV atualizado com sucesso!"
